2e1a0f3af545689497348efe2ff14d06ee53cb7b
Cortlandt Winters <cort@cortwinters.net>
2016-03-21 11:11:09 -0400

    run base scaffolds, still need to add refs to models and seed data

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,17 @@
 Rails.application.routes.draw do
+  resources :border_types
+  resources :regions_borders
+  resources :areas
+  resources :environment_types
+  resources :environments
+  resources :regions
+  resources :position_states
+  resources :positions
+  resources :position_states
+  resources :positions
+  resources :maps
+  resources :turns
+  resources :games
   root to: 'visitors#index'
   devise_for :users
   resources :users
